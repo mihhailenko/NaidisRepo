bd0e9f7eb9bbf15c35129ed15be057d654ea69eb
opilane <opilane@HP-CZC2349HTS>
2026-03-04 14:04:32 +0200

    Alustasid osa 4 funktsioonidega

---
+++ NaidisRepo/Osa4_funktsioonid.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaidisRepo
+{
+    public static class Osa4_funktsioonid
+    {
+        // 1. ÜL: Lemmiktoidu salvestamine faili (StreamWriter ja try-catch)
+        public static void LemmiktoiduSalvestamineFaili()
+        {
+            Console.Clear();
+            Console.WriteLine("1. ÜL: Lemmiktoidu salvestamine faili (StreamWriter)\n");
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Retseptid.txt");
+
+            Console.Write("Sisesta üks Itaalia toidu nimi (nt Lasagne või Risotto): ");
+            string toit = Console.ReadLine();
+
+             StreamWriter sw = new StreamWriter(path, true); // true = lisa lõppu
+             sw.WriteLine(toit);
+             sw.Close();
+
+             Console.WriteLine("Toit on salvestatud faili Retseptid.txt");
+            
+        }
+
+        // 2. ÜL: Kogu menüü kuvamine (StreamReader)
+        public static void KoguMenuuKuvamine()
+        {
+            Console.Clear();
+            Console.WriteLine("2. ÜL: Kogu menüü kuvamine (StreamReader)\n");
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Retseptid.txt");
+
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Faili Retseptid.txt ei ole olemas. Kõigepealt tee ÜL 1.");
+                return;
+            }
+
+            StreamReader sr = new StreamReader(path);
+            string sisu = sr.ReadToEnd();
+            sr.Close();
+
+            Console.WriteLine("Retseptid.txt sisu:\n");
+            Console.WriteLine(sisu);
+        }
+
+        // 3. ÜL: Koostisosade muutmine nimekirjas
+        public static void KoostisosadeMuutmine()
+        {
+            Console.Clear();
+            Console.WriteLine("3. ÜL: Koostisosade muutmine nimekirjas (List + File.ReadAllLines)\n");
+
+            List<string> koostisosad_list = LaeKoostisosad();
+
+            Console.WriteLine("Algne nimekiri:");
+            foreach (string rida in koostisosad_list)
+            {
+                Console.WriteLine(rida);
+            }
+
+            // Muuda esimest elementi
+            if (koostisosad_list.Count > 0)
+            {
+                koostisosad_list[0] = "Kvaliteetne oliiviõli";
+            }
+
+            // Eemalda "Ketšup"
+            koostisosad_list.Remove("Ketšup");
+
+            Console.WriteLine("\nUuenenud nimekiri:");
+            foreach (string rida in koostisosad_list)
+            {
+                Console.WriteLine(rida);
+            }
+        }
+
+        private static List<string> LaeKoostisosad()
+        {
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Koostisosad.txt");
+
+            List<string> koostisosad_list = new List<string>();
+            foreach (string rida in File.ReadAllLines(path))
+            {
+                koostisosad_list.Add(rida);
+            }
+
+            return koostisosad_list;
+        }
+    }
+}
--- NaidisRepo/StartPage.cs
+++ NaidisRepo/StartPage.cs
@@ -20,7 +20,7 @@ namespace NaidisRepo
                 Console.WriteLine("1. Osa Andmetüübid");
                 Console.WriteLine("2. Osa Funktsioonid");
                 Console.WriteLine("3. Osa Kordused, Listid ja Massiivid");
-                Console.WriteLine("4. Osa");
+                Console.WriteLine("4. Osa Failitöötlus");
                 Console.WriteLine("5. Osa");
                 Console.WriteLine("0. Välja");
 
@@ -39,7 +39,7 @@ namespace NaidisRepo
                         Kolmas_osa();
                         break;
                     case "4":
-                        Console.WriteLine("Ei ole see ülesanne veel valmis. Tulge tagasi hiljem.");
+                        Neljas_osa();
                         break;
                     case "5":
                         Console.WriteLine("Ei ole see ülesanne veel valmis. Tulge tagasi hiljem.");
@@ -109,11 +109,11 @@ namespace NaidisRepo
 
                 Console.WriteLine("1  - ÜL: Juku kino pilet");
                 Console.WriteLine("2  - ÜL: Kaks pinginaabrit");
-                Console.WriteLine("3  - ÜL: Ristküliku toa pindala + remondi hind");
+                Console.WriteLine("3  - ÜL: Ristküliku toa pindala   remondi hind");
                 Console.WriteLine("4  - ÜL: Leia alghind (30% soodustuse alghinnaga)");
                 Console.WriteLine("5  - ÜL: Temperatuur > 18?");
                 Console.WriteLine("6  - ÜL: Pikkuse kategooria - lühike / keskmine / pikk");
-                Console.WriteLine("7  - ÜL: Pikkus + sugu - lühike / keskmine / pikk");
+                Console.WriteLine("7  - ÜL: Pikkus   sugu - lühike / keskmine / pikk");
                 Console.WriteLine("8  - ÜL: Poes ostud");
                 Console.WriteLine("0  - Tagasi");
                 Console.WriteLine();
@@ -245,5 +245,56 @@ namespace NaidisRepo
                 Console.ReadLine();
             }
         }
-    }
-}
+
+        public static void Neljas_osa()
+         {
+             while (true)
+             {
+                 Console.Clear();
+                 Console.WriteLine("4. osa Failitöötlus ja Listid");
+                 Console.WriteLine("Milline ÜL kas te tahaksite üle vaadata?");
+                 Console.WriteLine();
+
+                 Console.WriteLine("1  - ÜL: Lemmiktoidu salvestamine faili (StreamWriter)");
+                 Console.WriteLine("2  - ÜL: Kogu menüü kuvamine (StreamReader)");
+                 Console.WriteLine("3  - ÜL: Koostisosade muutmine nimekirjas (List ja File.ReadAllLines)");
+                 Console.WriteLine("4  - ÜL: Külmkapi kontroll ehk otsing listist (Contains)");
+                 Console.WriteLine("5  - ÜL: Uuendatud nimekirja salvestamine (File.WriteAllLines)");
+                 Console.WriteLine("0  - Tagasi");
+                 Console.WriteLine();
+
+                 Console.Write("Sinu valik: ");
+                 string valik = Console.ReadLine();
+
+                 switch (valik)
+                 {
+                     case "1":
+                         Osa4_funktsioonid.LemmiktoiduSalvestamineFaili();
+                         break;
+                     case "2":
+                        Osa4_funktsioonid.KoguMenuuKuvamine();
+                        break;
+                     case "3":
+                         //Osa4_funktsioonid.KoostisosadeMuutmine();
+                         break;
+                     case "4":
+                         //Osa4_funktsioonid.KoostisosaOtsing();
+                         break;
+                     case "5":
+                         //Osa4_funktsioonid.KoostisosadeSalvestamine();
+                         break;
+                     case "0":
+                         return;
+
+                     default:
+                         Console.WriteLine("Tundmatu valik. Vajuta Enter...");
+                         Console.ReadLine();
+                         continue;
+                 }
+
+                 Console.WriteLine("\nVajuta Enter, et menüüsse tagasi minna...");
+                 Console.ReadLine();
+             }
+         }
+     }
+ }
